ba99dbf629da7e704e7aaef146de57a5cce0f3fc
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-06-30 13:17:26 +0100

    Feat: added status base mixin

---
+++ src/main/java/com/envyful/mixins/reforged/MixinStatusBase.java
@@ -0,0 +1,39 @@
+package com.envyful.mixins.reforged;
+
+import com.google.common.collect.Maps;
+import com.pixelmonmod.pixelmon.battles.status.NoStatus;
+import com.pixelmonmod.pixelmon.battles.status.StatusBase;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+
+@Mixin(StatusBase.class)
+public class MixinStatusBase {
+
+    private static final Map<Class<? extends StatusBase>, Constructor<? extends StatusBase>> CONSTRUCTORS_CACHE = Maps.newHashMap();
+
+    /**
+     *
+     * @reason Optimizes the {@link StatusBase} class' usage of reflection to get new instances of statuses
+     * @author danorris709
+     */
+    @Overwrite(remap = false)
+    public static StatusBase getNewInstance(Class<? extends StatusBase> statusClass) {
+        try {
+            return CONSTRUCTORS_CACHE.computeIfAbsent(statusClass, ___ -> {
+                try {
+                    return statusClass.getConstructor();
+                } catch (NoSuchMethodException e) {
+                    e.printStackTrace();
+                }
+
+                return null;
+            }).newInstance();
+        } catch (InstantiationException | InvocationTargetException | IllegalAccessException var2) {
+            return NoStatus.noStatus;
+        }
+    }
+}
