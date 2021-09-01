01399021a978e2d2b44f9f1b50afcd3253740212
danorris709 <dnorris443@gmail.com>
2021-09-01 08:08:13 +0100

    fix(mixins): missing update form method

--- src/main/java/com/envyful/mixins/reforged/MixinPokemonBase.java
+++ src/main/java/com/envyful/mixins/reforged/MixinPokemonBase.java
@@ -1,15 +1,20 @@
 package com.envyful.mixins.reforged;
 
 import com.pixelmonmod.pixelmon.api.pokemon.PokemonBase;
+import com.pixelmonmod.pixelmon.enums.EnumSpecies;
 import com.pixelmonmod.pixelmon.enums.forms.IEnumForm;
 import com.pixelmonmod.pixelmon.util.ITranslatable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PokemonBase.class)
 public abstract class MixinPokemonBase implements ITranslatable {
+
+    @Shadow public abstract EnumSpecies getSpecies();
 
     private IEnumForm formCache = null;
 
@@ -26,5 +31,15 @@ public abstract class MixinPokemonBase implements ITranslatable {
             cir.setReturnValue(this.formCache);
             cir.cancel();
         }
+    }
+
+    @Inject(method = "setForm(I)V", at = @At("HEAD"), remap = false, cancellable = true)
+    public void onSetFormIntHEAD(int form, CallbackInfo ci) {
+        this.formCache = this.getSpecies().getFormEnum(form);
+    }
+
+    @Inject(method = "setForm(Lcom/pixelmonmod/pixelmon/enums/forms/IEnumForm;)V", at = @At("HEAD"), remap = false, cancellable = true)
+    public void onSetFormEnumHEAD(IEnumForm form, CallbackInfo ci) {
+        this.formCache = form;
     }
 }
