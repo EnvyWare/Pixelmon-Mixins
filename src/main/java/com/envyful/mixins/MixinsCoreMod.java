28cff3abaaa7493df5f9042284f62549ef54d052
Daniel Norris <33832062+danorris709@users.noreply.github.com>
2021-10-21 17:23:07 +0100

    del(forge): remove tab manager mixins - use my mod

--- src/main/java/com/envyful/mixins/MixinsCoreMod.java
+++ src/main/java/com/envyful/mixins/MixinsCoreMod.java
@@ -117,7 +117,6 @@ public class MixinsCoreMod implements IFMLLoadingPlugin {
 
     public MixinsCoreMod() {
         addToCoremodList("com/pixelmonmod/pixelmon/api/pokemon/Pokemon.class", "mixins.pixelmon.json");
-        addToCoremodList("uk/co/proxying/tabmanager/TabManager.class", "mixins.tabmanager.json");
         addToCoremodList("com/pixelextras/PixelExtras.class", "mixins.pixelextras.json");
         addToCoremodList("ca/landonjw/gooeylibs2/GooeyLibs.class", "mixins.gooeylibs.json");
         addToCoremodList("com/mcsimonflash/sponge/activetime/ActiveTime.class", "mixins.activetime.json");
